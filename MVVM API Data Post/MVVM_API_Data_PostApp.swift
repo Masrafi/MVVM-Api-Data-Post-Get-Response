57ac767a214a00c4594ecc3688dca1cc040cfe40
Masrafi <masrafianam@gmail.com>
2024-07-29 17:11:20 +0600

    Initial Commit

---
+++ MVVM API Data Post/MVVM_API_Data_PostApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_API_Data_PostApp.swift
+//  MVVM API Data Post
+//
+//  Created by Md Khorshed Alam on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_API_Data_PostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
